fe79591e63fc931b143bf0ec197d44fa03ebf4c6
Racha <43282307+racha-rachou@users.noreply.github.com>
2020-12-30 16:09:31 +0100

    test Racha sysout

--- src/application/Main.java
+++ src/application/Main.java
@@ -19,7 +19,7 @@ public class Main extends Application {
 		primaryStage.setScene(scene);
 		primaryStage.show();
 			
-		
+		System.out.println("test Racha");
 	}
 	
 	public static void main(String[] args) {
