67e11152ee64387c86fbab2982ce9ea90dcdd0e9
Balkis <60971348+balkiscodes@users.noreply.github.com>
2020-12-30 15:16:48 +0100

    Update Main.java

--- src/application/Main.java
+++ src/application/Main.java
@@ -19,6 +19,7 @@ public class Main extends Application {
 		primaryStage.setScene(scene);
 		primaryStage.show();
 			
+		System.out.println("balkis workiiing");
 	}
 	
 	public static void main(String[] args) {
