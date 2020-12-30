62020139e025d68467e246c1c3bc179484789e7d
Balkis <60971348+balkiscodes@users.noreply.github.com>
2020-12-30 15:22:55 +0100

    Update Main.java
    changed msthg

--- src/application/Main.java
+++ src/application/Main.java
@@ -19,7 +19,7 @@ public class Main extends Application {
 		primaryStage.setScene(scene);
 		primaryStage.show();
 			
-		System.out.println("balkis workiiing");
+		System.out.println("balkis changed workiiing");
 	}
 	
 	public static void main(String[] args) {
