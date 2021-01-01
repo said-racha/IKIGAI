a279d8a6de68e29e666980bfb193c6ed52cc463d
Racha <43282307+racha-rachou@users.noreply.github.com>
2021-01-01 20:37:30 +0100

    interfaces non connectée

--- src/application/Main.java
+++ src/application/Main.java
@@ -13,10 +13,16 @@ import javafx.scene.layout.BorderPane;
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) throws IOException {
-		Parent root =FXMLLoader.load(getClass().getResource("Login.fxml"));
-		Scene scene = new Scene(root);
 		
+		//Parent root =FXMLLoader.load(getClass().getResource("Login.fxml"));
+		//Parent root2 =FXMLLoader.load(getClass().getResource("Inscription.fxml"));
+		//Parent root3 =FXMLLoader.load(getClass().getResource("ActiviteesDuJour.fxml"));
+		//Parent root4 =FXMLLoader.load(getClass().getResource("ConsulterDevPerso.fxml"));
+		Parent root5 =FXMLLoader.load(getClass().getResource("InfoApprises.fxml"));
+		
+		Scene scene = new Scene(root5);
 		primaryStage.setScene(scene);
+		
 		primaryStage.show();
 		
 	}
