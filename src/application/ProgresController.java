93adabc28633824a627981df3bb49ca803107670
Balkis <60971348+balkiscodes@users.noreply.github.com>
2021-01-02 13:03:53 +0100

    home 6

--- src/application/ProgresController.java
+++ src/application/ProgresController.java
@@ -1,16 +1,43 @@
 package application;
 
+import java.io.IOException;
+
+import javafx.animation.Interpolator;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.util.Duration;
 
 public class ProgresController {
 	  @FXML
 	    private Button home;
 
 	    @FXML
-	    void home(ActionEvent event) {
+	    void home(ActionEvent event) throws IOException {
+	    	Parent root =FXMLLoader.load(getClass().getResource("Home.fxml"));
+	        
+		   	   Scene scene = home.getScene();
+		          root.translateYProperty().set(scene.getHeight());
 
+		          AnchorPane parentcontainer = (AnchorPane) scene.getRoot();
+
+		          parentcontainer.getChildren().add(root);
+
+		          Timeline timeline = new Timeline();
+		          KeyValue kv = new KeyValue(root.translateYProperty(), 0, Interpolator.EASE_IN);
+		          KeyFrame kf = new KeyFrame(Duration.seconds(1), kv);
+		          timeline.getKeyFrames().add(kf);
+		          timeline.setOnFinished(t -> {
+		              parentcontainer.getChildren().remove(parentcontainer);
+		          });
+		          timeline.play();
 	    }
 
 
