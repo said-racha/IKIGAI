260437a984074711a3db247eb7d1a21837880267
Balkis <60971348+balkiscodes@users.noreply.github.com>
2021-01-02 12:33:25 +0100

    suite

--- src/application/HomeController.java
+++ src/application/HomeController.java
@@ -1,7 +1,30 @@
 package application;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 public class HomeController {
+	@FXML
+    private Button pp;
 
+    @FXML
+    private Button dev;
+
+    @FXML
+    private Button sh;
+
+    @FXML
+    private Button aj;
+
+    @FXML
+    private Button progres;
+
+    @FXML
+    private Button sport;
+
+    @FXML
+    private Button ed;
+
+    @FXML
+    private Button appris;
 }
