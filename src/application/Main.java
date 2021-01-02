5138fd416519329912aa9d97d7b5222a02873f5f
Balkis <60971348+balkiscodes@users.noreply.github.com>
2021-01-02 20:41:44 +0100

    added stylesheet in main

--- src/application/Main.java
+++ src/application/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) throws IOException {
 		Parent root =FXMLLoader.load(getClass().getResource("Home.fxml"));
 		Scene scene = new Scene(root);
-		
+		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 		primaryStage.setScene(scene);
 		primaryStage.show();
 		
