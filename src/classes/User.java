86dd1a2e452cb9f64cf643b3116d02b11be3f2f5
Balkis <60971348+balkiscodes@users.noreply.github.com>
2021-01-11 23:41:52 +0100

    coded settings
    still has to be updated tho:)

--- src/classes/User.java
+++ src/classes/User.java
@@ -2,10 +2,12 @@ package classes;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Date;
 
 import DbUtil.DbConnection;
+
 
 public class User {
 	
@@ -52,5 +54,48 @@ public class User {
 		SessionSkill = sessionSkill;
 	}
 	
+	 //****=============FONCTION QUI PERMET DE NOUS CONNECTER A LA BDD===========
+    Connection connection;
+
+
+
+    public boolean isDataBaseConnected() {
+        try {
+
+            this.connection= DbConnection.getConnection();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return this.connection != null;
+    }
+    
+    //****=============FONCTION QUI PERMET DE MODIFIER UN USER DANS LA BDD===========
+    public void ModifierUSR(String fullName, String Oldmail,String Email, String mdp, String skill) {
+
+        String query ="UPDATE USER SET fullName = ?, email = ? , mdp = ?,skill  =? WHERE email = ? ";
+
+        
+        try (PreparedStatement pr = this.connection.prepareStatement(query)) {
+            pr.setString(1,  fullName);
+            //Nouveau email
+            pr.setString(2, Email);
+            pr.setString(3, mdp);
+            pr.setString(4, skill);
+            pr.setString(5, Oldmail);
+            
+       
+            
+            pr.executeUpdate();
+            //les seuls valeurs a modifier dans un RDV sont la date et l'heure
+        } catch (SQLException e) {
+            System.out.println("Vous avez un probleme dans la classe User Modifier User");
+        }
+
+    }
+
+
+
+ 
 
 }
