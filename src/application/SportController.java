82a346570b6d09ba48409fa9e305b4de8b443387
Racha <43282307+racha-rachou@users.noreply.github.com>
2021-01-13 18:45:53 +0100

    rub sport mais manque les videos f bdd

--- src/application/SportController.java
+++ src/application/SportController.java
@@ -2,13 +2,18 @@ package application;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
+import classes.Sport;
 import javafx.animation.Interpolator;
 import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
@@ -16,22 +21,45 @@ import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
 import javafx.util.Duration;
 
-public class SportController {
+public class SportController implements Initializable{
+	
 	@FXML
 	private AnchorPane container;
 	@FXML
 	private Button backBtn;
 	@FXML
-	private Button validerBtn;
-	@FXML
-    private TextArea sportTextA;
-
-	// Event Listener on Button[#backBtn].onAction
+    private Label ThemeDuJour;
+	
+	private int nbrJour;
+	private String programmeDuJour;
+	private String themeDuJour;
+	
+	
+	@FXML private WebView webView;
+	@FXML private WebEngine engine;// c'est ce qui va me permettre de charger notre webView (charge la val entrée a engine pr l'affiché dans le webview
+	
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		
+		nbrJour=3; //a remplacer par user.getNbrJour();
+		ThemeDuJour.setText(Sport.getThemeDuJour(nbrJour));
+		
+		engine= webView.getEngine();
+		engine.load(Sport.getProgrammeDuJour(nbrJour));
+		
+	}
+	
+	
 	@FXML
 	public void Back(ActionEvent event) throws IOException {
 		
+		engine.load(null);//fermer le webview
+    	
 		Parent root =FXMLLoader.load(getClass().getResource("Home.fxml"));
 
         
@@ -51,4 +79,9 @@ public class SportController {
 	         });
 	         timeline.play();
 	}
+
+
+
+
+	
 }
